bc69e30bc66e6630f9ca6bdbb57d41d8a78c3058
Rafael Morais <rafaelm.morais@hotmail.com>
2024-10-15 14:53:15 -0300

    adicionado reportportal

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,27 @@
 const { defineConfig } = require("cypress");
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin');
 
 module.exports = defineConfig({
+  reporter: '@reportportal/agent-js-cypress',
+  reporterOptions: {
+    apiKey: 'ebac-shop_HB_lTc8FR-aHs4xEAX7sW_DPgzntnnIApFqyTCDSE60QPosvh6uh5Lg1Gmdb2Udi',
+    endpoint: 'https://demo.reportportal.io/api/v1',
+    project: 'rafaelmmorais_personal',
+    launch: 'Testes em cypress',
+    description: 'teste da ebac shop',
+    attributes: [
+      {
+        key: 'attributeKey',
+        value: 'attributeValue',
+      },
+      {
+        value: 'anotherAttributeValue',
+      },
+    ],
+  },
   e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
-      
-      require('cypress-html-reporter/GenerateReport')(on, config)
-
+      return registerReportPortalPlugin(on, config);
     },
   },
-
-  /*
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportFilename: "[name]-result",
-    html: false
-  }
-  */
-
 });
---
+++ cypress/plugins/index.js
@@ -0,0 +1,4 @@
+
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin');
+
+module.exports = (on, config) => registerReportPortalPlugin(on, config);
